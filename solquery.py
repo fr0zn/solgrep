6c8c39b64b18a9aa9ec76601c31a26b37ec3a5fb
fr0zn <ferran.celades@hotmail.com>
2021-11-21 17:22:50 +0100

    Base working

--- solquery.py
+++ solquery.py
@@ -40,6 +40,7 @@ class TreeNode(NodeMixin):
         self.content = content
         self.parent = parent
         self.is_named = node.is_named if node else False
+        self.is_meta = False
         if children:
             self.children = children
     
@@ -66,7 +67,7 @@ class TreeRoot():
 
         self._sexp = '' 
 
-        self.metavars = {}
+        # self.metavars = {}
 
         self._prepare_traverse()
 
@@ -91,11 +92,12 @@ class TreeRoot():
         node = TreeNode(_node.type, _node, self._src[_node.start_byte:_node.end_byte], parent=_last_parent)
         if self.root == None:
             self.root = node
-        if node.type == 'identifier':
-            _identifier = node.content
-            if _identifier.startswith('$'):
-                if _identifier not in self.metavars: 
-                    self.metavars[_identifier] = MetaVar(node)
+        # if node.type == 'identifier':
+        #     _identifier = node.content
+        #     if _identifier.startswith('$'):
+        #         node.is_meta = True
+        #         if _identifier not in self.metavars: 
+        #             self.metavars[_identifier] = None #MetaVar(node)
         return node
  
     def _prepare_traverse(self):
@@ -115,13 +117,13 @@ class TreeRoot():
     # def __repr__(self) -> str:
     #     return self.__str__()
 
-class MetaVar():
-    def __init__(self, node):
-        self.node = node
-        self.value = None
+# class MetaVar():
+#     def __init__(self, node):
+#         self.node = node
+#         self.value = None
     
-    def __repr__(self):
-        return "{}({})".format(self.node.content, self.value)
+#     def __repr__(self):
+#         return "{}({})".format(self.node.content, self.value)
 
 class SolidityQuery():
     def __init__(self):
@@ -131,9 +133,10 @@ class SolidityQuery():
 
         self.src = None
         self.queries = None
-        # self.queries = []
 
-        # self.metavars = {}
+        # A dictionary of META keys and values
+        self.query_metavars = []
+        self.query_metavars_index = 0
 
     def _build_load_library(self):
         Language.build_library('build/solidity.so',[ '.' ])
@@ -142,7 +145,8 @@ class SolidityQuery():
     def _parse_file(self, fileName):
         _content = open(fileName).read()
         # Append and prepend ellipsis to the query
-        _content = '...\n' + _content + '\n...'
+        # _content = '...\n' + _content + '\n...'
+        # _content = _content + '\n...'
         return (_content, self.parser.parse(bytes(_content, 'utf8')))
 
     def load_source(self, fileName):
@@ -156,17 +160,14 @@ class SolidityQuery():
         # TODO: Checks MISSING ERROR
         self.queries = TreeRoot(_content, _tree.root_node)
         return self.queries
-        for child in _tree.root_node.children:
-            self.queries.append(TreeRoot(_content, child))
-        return self.queries
     
-    def _parse_query(self, query_root):
-        for query_node in PreOrderIter(query_root):
-            if query_node.type == 'identifier':
-                _content = query_node.content
-                if query_node.content not in self.metavars:
-                    self.metavars[_content] = MetaVar(query_node)
-            # print(_content)
+    # def _parse_query(self, query_root):
+    #     for query_node in PreOrderIter(query_root):
+    #         if query_node.type == 'identifier':
+    #             _content = query_node.content
+    #             if query_node.content not in self.metavars:
+    #                 self.metavars[_content] = None #MetaVar(query_node)
+    #         # print(_content)
 
     # def get_node_content(self, node, src):
     #     return src[node.start_byte:node.end_byte]
@@ -175,8 +176,40 @@ class SolidityQuery():
     #     self.src
     #     print(capture)
 
-    def _compareNodes(self, node1, node2):
-        return node1.type == node2.type
+    def _compareNodes(self, searchNode, compareNode, added_meta):
+        # The compareNode can be an ellipsis
+        if searchNode.type == compareNode.type:
+            _gtype = searchNode.type
+            if _gtype == 'ellipsis':
+                return True
+            elif _gtype == 'identifier':
+                # If the search content starts with $, thats a METAVAR
+                if searchNode.content.startswith('$'):
+                    _metavar = searchNode.content
+                    _content = compareNode.content
+                    if _metavar not in self.query_metavars[self.query_metavars_index]:
+                        added_meta.append(_metavar)
+                        self.query_metavars[self.query_metavars_index][_metavar] = _content
+                        return True
+                    else:
+                        # The node is equal if the metavar is the same
+                        return self.query_metavars[self.query_metavars_index][_metavar] == _content
+                # True if both identifiers are the same
+                return searchNode.content == compareNode.content
+
+            return True
+        else:
+            return False
+    
+    # def _beforeSkipFunction(self):
+    #     print(self.query_metavars)
+    #     self._cache_query_metavars = self.query_metavars.copy()
+
+    # def _afterSkipFunction(self):
+    #     print(self._cache_query_metavars)
+    #     print(self._cache_query_metavars)
+    #     self.query_metavars = {}
+    #     # self.query_metavars = self._cache_query_metavars.copy()
     
     def _do_query(self):
         ellipsis_node = TreeNode('ellipsis', None, '...')
@@ -186,47 +219,55 @@ class SolidityQuery():
         query_first_rule = None
         # Skip until the find the first rule of the query that is not an ellipsis
         for child in self.queries.root.children:
-            if not self._compareNodes(child, ellipsis_node):
+            if not self._compareNodes(child, ellipsis_node, []):
                 query_first_rule = child
                 break
 
         _matches = []
         for src_node in PreOrderIter(self.src.root):
-            if src_node.name == query_first_rule.name:
-                _root_src = src_node.parent
+            # print('===========')
+            # print(src_node)
+            # print(query_first_rule)
+            # print('===========')
+            if src_node.type == query_first_rule.type:
+                self.query_metavars.append({})
+                _src_parent = src_node.parent
                 print('=========== SRC TREE ============')
-                print(str(RenderTree(_root_src)))
+                print(str(RenderTree(_src_parent)))
                 print('=========== SRC CONTENT ============')
-                print(_root_src.content)
+                print(src_node.content)
                 print()
                 print('=========== QUERY TREE ============')
                 print(self.queries)
                 print('=========== QUERY CONTENT ============')
                 print(self.queries.root.content)
-                _match = compare_levels(_root_src, self.queries.root, 
+                # We are queriying using the full queries root content
+                # We use the parent of the found src node for the first query
+                # rule but skipped n times, where n is the index of the found
+                # child 
+                _match = compare_levels(_src_parent, self.queries.root, 
                         ellipsisNode=ellipsis_node,
                         compareFunction=self._compareNodes,
+                        srcIndexStart=_src_parent.children.index(src_node),
+                        metaVars=self.query_metavars[self.query_metavars_index]
                         )
+                self.query_metavars_index += 1
+
                 print('=========== MATCH ============')
                 print(_match)
                 _matches.append(_match)
                 print('==============================')
+                # break
 
         print('=============')
         print(_matches)
-        print(self.queries.metavars)
+        print(self.query_metavars)
         print('=============')
 
     def query(self):
         captures = None
         # for i,query in enumerate(self.queries):
         self._do_query()
-            # query_sexp = query.filter_type(['ellipsis']) + '@query{}'.format(i)
-            # print(query_sexp)
-            # query = self.SOLIDITY_LANGUAGE.query(query_sexp)
-            # captures = query.captures(self.src._root)
-            # for capture in captures:
-            #     self._parse_captures(capture)
 
         return captures
         print(sexp_format(query_sexp))
--- solquery_compare.py
+++ solquery_compare.py
@@ -1,6 +1,6 @@
 # This function assumes that _src_root and _search_root are equal
-def compare_levels(_src_root, _search_root, ellipsisNode, compareFunction):
-    _src_children = _src_root.children
+def compare_levels(_src_root, _search_root, ellipsisNode, compareFunction, srcIndexStart=0, metaVars={}):
+    _src_children = _src_root.children[srcIndexStart:]
     _search_children = _search_root.children
     src_index = 0
     search_index = 0
@@ -11,9 +11,10 @@ def compare_levels(_src_root, _search_root, ellipsisNode, compareFunction):
         return True
     # for e in search:
     # Iterate over all src nodes
+    added_meta = []
     while search_index < len(_search_children):
         
-        if compareFunction(_search_children[search_index], ellipsisNode):
+        if compareFunction(_search_children[search_index], ellipsisNode, []):
             in_ellipsis = True
             search_index += 1
             continue
@@ -21,19 +22,42 @@ def compare_levels(_src_root, _search_root, ellipsisNode, compareFunction):
         # None ellipsis element on search
         # check for match, if no match increment src index
         # if match, continue with next search
+
+        # If we have more search nodes than src nodes
+        # That means we are exhausting the src length
+        if src_index >= len(_src_children):
+            return False
         
-        if compareFunction(_search_children[search_index], _src_children[src_index]):
+        if compareFunction(_search_children[search_index], _src_children[src_index], added_meta):
             _match = compare_levels(
                 _src_children[src_index], 
                 _search_children[search_index],
                 ellipsisNode=ellipsisNode,
-                compareFunction=compareFunction
+                compareFunction=compareFunction,
+                srcIndexStart=0,
+                metaVars=metaVars
                 )
+            # If this branch produces no match, try with other src childs
             if not _match:
-                return False
-            search_index += 1
-            src_index += 1
-            in_ellipsis = False
+                if in_ellipsis:
+                    # We will need to remove created metavars for the current branch
+                    # if not a full match
+                    for added in added_meta:
+                        metaVars.pop(added)
+                    # beforeMetaVar = dict(metaVars)
+                    # print('A',metaVars)
+                    # metaVars = {}
+                    src_index += 1
+                else:
+                    return False
+                # return False
+            else:
+                # metaVars.update(tmp_metavar)
+                # metaVars = dict(metaVars.items() & tmp_metavar.items())
+                # tmp_metavar = metaVars.copy()
+                search_index += 1
+                src_index += 1
+                in_ellipsis = False
         # Increment src index until we find the search node
         else:
             # If we are in an ellipse, just continue to look on src
@@ -44,13 +68,13 @@ def compare_levels(_src_root, _search_root, ellipsisNode, compareFunction):
             else:
                 return False
 
-            # If we have more search nodes than src nodes
-            if src_index >= len(_src_children):
-                return False
 
-    # If we are still missing some nodes to inspect on the
-    # src and we don't end with an ellipsis, that not a full match
+    # If we are still missing some nodes to inspect on src
+    # and we don't end with an ellipsis, thats not a full match unless
+    # we completed the search
     if not in_ellipsis and len(_src_children) > src_index:
+        if search_index >= len(_search_children):
+            return True
         return False
         
     return True
